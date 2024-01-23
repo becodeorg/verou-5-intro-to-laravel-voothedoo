238b625eefbea683056ffef6b56ccb46e765f0e7
Alex Munteanu <vootehdoo@gmail.com>
2024-01-23 10:45:20 +0100

    Access the  params with Illuminate\Http\Request

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,9 +6,12 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return view ('welcome');
+        return view('welcome');
+        //----for http://127.0.0.1:8000/?param1=hello it will echo hello----
+        // $param1 = $request->query('param1');
+        // echo $param1;
     }
 
     public function pageOne()
